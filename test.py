c41be8b427601649c7840692930dd01b5c8b8760
inaniwaudon <inaniwaudon@users.noreply.github.com>
2025-04-14 18:42:26 +0000

    Auto-format with ruff

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import requests
 
+
 def fetch_example_data():
     try:
-        response = (
-            requests.get('https://example.com')
-        )
+        response = requests.get("https://example.com")
         response.raise_for_status()  # HTTPエラーが発生した場合に例外を発生させる
         return response.text
     except requests.RequestException as e:
         print(f"エラーが発生しました: {e}")
         return None
+
 
 if __name__ == "__main__":
     data = fetch_example_data()
